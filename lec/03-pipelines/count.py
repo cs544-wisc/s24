349a37b5f1af2ee00ea0946bcde6b46a8c2cd889
Meenakshi Syamkumar <msyamkumar@wisc.edu>
2024-01-31 12:27:21 -0600

    Lec 4 files

--- lec/03-pipelines/count.py
+++ lec/03-pipelines/count.py
@@ -1,17 +1,19 @@
-#!/usr/bin/python3
+#! /usr/bin/python3
 
 count = 0
 
-from pathlib import Path
+from pathlib import Path, PurePath
 import time
 
 # absolute path: /home/meenakshisyamkumar/data/stations.txt
 # relative path: data/stations.txt
 # Problem: Code wouldn't work on Windows!
 
-with open(Path("data") / "stations.txt") as f:
+with open(PurePath(Path("data"), Path("sample")) / "stations.txt") as f:
     for line in f:
         count = count + 1
         time.sleep(1) # sleep for 1 second
+        print(count)
 
 print(count)
+
