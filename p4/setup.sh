2359ef08395ac9e2b7e4cccf6f7fce9ddf81df99
Seth Ockerman <sockerman@wisc.edu>
2024-03-04 18:34:15 -0500

    Added wget commands, so it downloads needed grader files/updates them (#29)

--- p4/setup.sh
+++ p4/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+wget https://raw.githubusercontent.com/cs544-wisc/s24/main/p4/autograde.py -O autograde.py
+wget https://raw.githubusercontent.com/cs544-wisc/s24/main/nbutils.py -O nbutils.py
+wget https://raw.githubusercontent.com/cs544-wisc/s24/main/tester.py -O tester.py
+
 sudo apt-get update
 sudo apt install python3-pip
 pip3 install pandas docker
