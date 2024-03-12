e7e732e9f0bfa1ddd0b55b0579c339c528eba8a9
spencrr <23708360+spencrr@users.noreply.github.com>
2024-03-12 15:10:11 -0500

    p3: amend setup.sh

--- p3/setup.sh
+++ p3/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This is for AUTOGRADING ONLY
-pushd submission
-cp -R ../{workload,{autograde,../tester}.py} .
+SUBMISSION="submission"
+cp -R {workload,{autograde,../tester}.py} $SUBMISSION
+pushd $SUBMISSION
 python3 -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. mathdb.proto
 popd
