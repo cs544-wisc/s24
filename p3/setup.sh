d7c664706191671128748f00defd3b3c57c74a1b
qusuyan <qusuyan@gmail.com>
2024-03-15 05:41:52 +0000

    remove grading files

--- p3/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This is for AUTOGRADING ONLY
-SUBMISSION="submission"
-cp -R {workload,{autograde,../tester}.py} $SUBMISSION
-pushd $SUBMISSION
-python3 -m grpc_tools.protoc -I=. --python_out=. --grpc_python_out=. mathdb.proto
-popd
