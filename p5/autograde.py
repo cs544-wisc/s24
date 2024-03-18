73b04c2e3828bea14280ba9aa0c091dd153ed444
Suyan Qu <36519575+qusuyan@users.noreply.github.com>
2024-03-18 10:50:20 -0500

    P5 spark (#45)
    Co-authored-by: Md. Tareq Mahmood <tareq.py@gmail.com>

--- p5/autograde.py
+++ p5/autograde.py
@@ -1,54 +1,96 @@
-import json
-import re  # parsing JSON and regular expressions
+import subprocess, time, os, argparse
+import json, re # parsing JSON and regular expressions
 
-from tester import init, test, tester_main
+from tester import init, test, tester_main, cleanup, error, warn, verbose, run_with_timeout, get_args
 import nbutils
 
-ANSWERS = {}  # global variable to store answers { key = question number, value = output of the answer cell }
+import argparse
+
+ANSWERS = {} # global variable to store answers { key = question number, value = output of the answer cell }
+
+@cleanup
+def docker_cleanup():
+    args = get_args()
+    if args.skip_run:
+        return
+    
+    try:
+        subprocess.run(["docker compose kill; docker compose rm -f"], shell=True)
+        subprocess.run(["docker", "rmi", "-f", "p5-base", "p5-nb", "p5-nn", "p5-dn", "p5-boss", "p5-worker"], check=True, shell=False)
+        subprocess.run(["docker", "system", "prune", "-f"], check=True, shell=False)
+
+        # remove any other running container in case 
+        result = subprocess.run(["docker", "container", "ls", "-a"], capture_output = True, check=True, shell=False)
+        stdout = result.stdout.decode('utf-8')
+        if stdout.count("\n") > 1:
+            warn(f"stopping other running containers to free up spaces:\n {stdout}")
+            subprocess.run(["docker stop $(docker ps -a -q)"], check=True, shell=True)
+            subprocess.run(["docker rm -f $(docker ps -a -q)"], check=True, shell=True)
+    except:
+        pass
+
+
+def docker_startup():
+    args = get_args()
+    if args.skip_run:
+        return
+    
+    print("================================== building docker images ==================================")
+    subprocess.run(["docker", "build", ".", "-f", "p5-base.Dockerfile", "-t", "p5-base"])
+    subprocess.run(["docker", "build", ".", "-f", "notebook.Dockerfile", "-t", "p5-nb"])
+    subprocess.run(["docker", "build", ".", "-f", "namenode.Dockerfile", "-t", "p5-nn"])
+    subprocess.run(["docker", "build", ".", "-f", "datanode.Dockerfile", "-t", "p5-dn"])
+    subprocess.run(["docker", "build", ".", "-f", "boss.Dockerfile", "-t", "p5-boss"])
+    subprocess.run(["docker", "build", ".", "-f", "worker.Dockerfile", "-t", "p5-worker"])
+
+    print("================================== starting docker compose cluster ==================================")
+    subprocess.run(["docker", "compose", "up", "-d"])
+
+    check_hdfs_cmd = ["docker", "exec", "notebook", "hdfs", "dfsadmin", "-fs", "hdfs://nn:9000", "-report"]
+    while True:
+        try:
+            output = subprocess.check_output(check_hdfs_cmd)
+            m = re.search(r"Live datanodes \((\d+)\)", str(output, "utf-8"))
+            if not m:
+                verbose("report didn't describe live nodes")
+            else:
+                count = int(m.group(1))
+                if count > 0:
+                    print("HDFS ready!")
+                    break
+        except:
+            verbose("couldn't get report from namenode")
+        print("HDFS not ready ...")
+        time.sleep(5)
+
+
+def run_notebook():
+    print("================================== running jupyter notebook ==================================")
+    run_notebook_cmd = 'export CLASSPATH=`$HADOOP_HOME/bin/hdfs classpath --glob` && python3 -m nbconvert --execute --to notebook nb/p5.ipynb --output tester-p5.ipynb'
+    subprocess.run(["docker", "exec", "notebook", "sh", "-c", run_notebook_cmd])
+
+
+def collect_cells(tester_nb='nb/tester-p5.ipynb'):
+    global ANSWERS
+    if os.path.exists(tester_nb):
+        ANSWERS = nbutils.collect_answers(tester_nb)
 
 
 @init
-def collect_cells(*args, **kwargs):
-    with open("nb/p5.ipynb") as f:
-        nb = json.load(f)  # load the notebook as a json object
-        cells = nb["cells"]  # get the list of cells from the notebook
-        expected_exec_count = 1  # expected execution count of the next cell
-
-        for cell in cells:
-            if "execution_count" in cell and cell["execution_count"]:
-                exec_count = cell["execution_count"]
-                if exec_count != expected_exec_count:
-                    raise Exception(
-                        f"Expected execution count {expected_exec_count} but found {exec_count}. Please do Restart & Run all then save before running the tester.")
-                expected_exec_count = exec_count + 1
-
-            if cell["cell_type"] != "code":
-                continue
-
-            if not cell["source"]:
-                # if the cell is empty, skip it (empty = it has no source code)
-                continue
-
-            # pattern should be #q1 or #Q1 (#q2 or #Q2, etc.)
-            m = re.match(r"#[qQ](\d+)(.*)", cell["source"][0].strip())
-            if not m:
-                continue
-
-            # found a answer cell, add its output to list
-            qnum = int(m.group(1))
-            notes = m.group(2).strip()
-            if qnum in ANSWERS:
-                raise Exception(f"Answer {qnum} repeated!")
-            # expected qnum = 1 + (max key in ANSWERS dictionary if ANSWERS is not empty else 0)
-            expected = 1 + (max(ANSWERS.keys()) if ANSWERS else 0)
-            if qnum != expected:
-                print(
-                    f"Warning: Expected question {expected} next but found {qnum}!")
-
-            # add the output of the answer cell to the ANSWERS dictionary
-            ANSWERS[qnum] = cell["outputs"]
-
-    # print("ANSWERS[7] = ", ANSWERS[7])
+def init():
+    args = get_args()
+    if args.skip_run:
+        collect_cells('nb/p5.ipynb')
+    else:
+        check_system_health()
+        def wrapper():
+            docker_cleanup()
+            docker_startup()
+            run_notebook()
+        err = run_with_timeout(wrapper, 1200)
+        if err:
+            return err
+        collect_cells()
 
 
 @test(points=10)
@@ -57,9 +99,8 @@ def q1():
         raise Exception("Answer to question 1 not found")
     outputs = ANSWERS[1]
     output = nbutils.parse_int_output(outputs)
-    if not nbutils.compare_int(525, output):
+    if not nbutils.compare_int(106, output):
         return "Wrong answer"
-
 
 @test(points=10)
 def q2():
@@ -67,9 +108,8 @@ def q2():
         raise Exception("Answer to question 2 not found")
     outputs = ANSWERS[2]
     output = nbutils.parse_int_output(outputs)
-    if not nbutils.compare_int(525, output):
+    if not nbutils.compare_int(106, output):
         return "Wrong answer"
-
 
 @test(points=10)
 def q3():
@@ -77,35 +117,33 @@ def q3():
         raise Exception("Answer to question 3 not found")
     outputs = ANSWERS[3]
     output = nbutils.parse_int_output(outputs)
-    if not nbutils.compare_int(525, output):
+    if not nbutils.compare_int(106, output):
         return "Wrong answer"
 
 
-# desugars to test(points=10)(q1) = wrapper(q1) -> TESTS["q1"] = _unit_test(q1, 10, None, "")
-@test(points=10)
+@test(points=10) # desugars to test(points=10)(q1) = wrapper(q1) -> TESTS["q1"] = _unit_test(q1, 10, None, "") 
 def q4():
     if not 4 in ANSWERS:
         raise Exception("Answer to question 4 not found")
     outputs = ANSWERS[4]
-    # print("ANSWERS[4] = ", ANSWERS[4])
     output = nbutils.parse_dict_bool_output(outputs)
-
+    
     if not nbutils.compare_dict_bools(
-        {'banks': False,
-         'loans': False,
-         'action_taken': True,
-         'counties': True,
-         'denial_reason': True,
-         'ethnicity': True,
-         'loan_purpose': True,
-         'loan_type': True,
-         'preapproval': True,
-         'property_type': True,
-         'race': True,
-         'sex': True,
-         'states': True,
-         'tracts': True
-         }, output):
+        {'banks': False, 
+        'loans': False, 
+        'action_taken': True, 
+        'counties': True, 
+        'denial_reason': True, 
+        'ethnicity': True, 
+        'loan_purpose': True, 
+        'loan_type': True, 
+        'preapproval': True, 
+        'property_type': True, 
+        'race': True, 
+        'sex': True, 
+        'states': True, 
+        'tracts': True
+        }, output):
         return "Wrong answer"
 
 
@@ -116,9 +154,8 @@ def q5():
     outputs = ANSWERS[5]
     # print("test 5 outputs: ", outputs)
     output = nbutils.parse_int_output(outputs)
-    if not nbutils.compare_int(19739, output):
+    if not nbutils.compare_int(6, output):
         return "Wrong answer"
-
 
 @test(points=10)
 def q6():
@@ -133,30 +170,31 @@ def q7():
         raise Exception("Answer to question 7 not found")
     outputs = ANSWERS[7]
     # print("ANSWERS[7] = ", ANSWERS[7])
-    output = nbutils.parse_dict_float_output(outputs)
-
-    if not nbutils.compare_dict_floats(
-        {'Milwaukee': 3.1173465727097907,
-         'Waukesha': 2.8758225602027756,
-         'Washington': 2.851009389671362,
-         'Dane': 2.890674955595027,
-         'Brown': 3.010949119373777,
-         'Racine': 3.099783715012723,
-         'Outagamie': 2.979661835748792,
-         'Winnebago': 3.0284761904761908,
-         'Ozaukee': 2.8673765432098772,
-         'Sheboygan': 2.995511111111111
-         }, output):
+    output = nbutils.parse_dict_int_output(outputs)
+    
+    if not nbutils.compare_dict_ints(
+    {
+        'Sawyer': 38,
+        'Door': 174,
+        'Forest': 7,
+        'Ozaukee': 389,
+        'Bayfield': 33,
+        'Waukesha': 1832,
+        'Vilas': 68,
+        'Dane': 729,
+        'Oneida': 70,
+        'Florence': 8
+    }, output):
         return "Wrong answer"
-
+        
 
 @test(points=10)
 def q8():
     if not 8 in ANSWERS:
         raise Exception("Answer to question 8 not found")
     # to be manually graded
-
-
+    
+    
 @test(points=10)
 def q9():
     if not 9 in ANSWERS:
@@ -166,16 +204,48 @@ def q9():
     if not nbutils.compare_float(242868.0, output):
         return "Wrong answer"
 
-
 @test(points=10)
 def q10():
     if not 10 in ANSWERS:
         raise Exception("Answer to question 10 not found")
     outputs = ANSWERS[10]
     output = nbutils.parse_float_output(outputs)
-    if not nbutils.is_accurate(0.86, output):
+    if not nbutils.is_accurate(0.89, output):
         return "Wrong answer"
-
+    
+def check_system_health():
+    try:
+        # swap
+        output = subprocess.check_output(['free', '-m']).decode('utf-8')
+        lines = output.split('\n')
+        swap_line = lines[2]
+        swap_info = swap_line.split()
+        total, used, free = int(swap_info[1]), int(swap_info[2]), int(swap_info[3])
+        if total < 1000:
+            error(f"Swap size ({total} MB) is much less than 1GB")
+            raise Exception("Swap size is less than recommended")
+        
+        # disk
+        output = subprocess.check_output(['df', '-h', '/']).decode('utf-8')
+        usage_line = output.split('\n')[1]
+        disk_usage = int(usage_line.split()[4][:-1])
+        if disk_usage > 70:
+            warn(f"Disk usage is {disk_usage}%")
+            warn("If autograder fails, consider cleaning docker system storage")
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
-    tester_main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--skip-run', action='store_true', help='Check answer without running the system')
+    tester_main(parser, required_files=[
+        "nb/p5.ipynb", 
+        "boss.Dockerfile",
+        "worker.Dockerfile",
+        "namenode.Dockerfile",
+        "datanode.Dockerfile",
+        "notebook.Dockerfile",
+        "p5-base.Dockerfile",
+        "docker-compose.yml",
+    ])
